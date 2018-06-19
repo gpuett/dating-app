3f93d70438768f3e48364c48d1c329ca7ebcceaf
Ami <example@example.com>
2018-06-19 14:24:18 -0700

    get profiles to swipe component

--- src/app/swipe/swipe.component.ts
+++ src/app/swipe/swipe.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { User } from '../user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-swipe',
   templateUrl: './swipe.component.html',
-  styleUrls: ['./swipe.component.scss']
+  styleUrls: ['./swipe.component.scss'],
+  providers: [DataService]
 })
 export class SwipeComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
+
+  getUsers() {
+    return this.dataService.getUsers().then(users => this.users = users);
+  }
 
   ngOnInit() {
+    this.getUsers();
+    console.log(this.dataService.getUsers())
   }
-  
+
 }
