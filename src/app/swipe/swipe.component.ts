f023a921df8c1b6afd6013d5eee02a4ba8a02cc7
Bobby Martin <robert.martin.619@gmail.com>
2018-06-20 16:56:42 -0700

    Slightly working functionality

--- src/app/swipe/swipe.component.ts
+++ src/app/swipe/swipe.component.ts
@@ -12,11 +12,18 @@ import { FilterPipe } from '../filter.pipe';
 })
 export class SwipeComponent implements OnInit {
   users;
-  
+  liked = false;
+
   constructor(private dataService: DataService) { }
 
   getUsers() {
     return this.dataService.getUsers().then(users => this.users = users);
+  }
+  likeUser() {
+    const active = document.querySelector('.swiper');
+    const pos = (num) => num += 100;
+    const isActive = () => active.setAttribute('style', "transform: translateX(-" + pos(100) + '%)');
+    isActive();
   }
 
   ngOnInit() {
