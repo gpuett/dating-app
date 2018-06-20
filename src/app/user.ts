ba5d0d99957b9d534b6e2c981a9dc079f2164122
Garnett Puett and semhar kidane <semhar@>
2018-06-20 15:46:46 -0700

    test with hide function

--- src/app/user.ts
+++ src/app/user.ts
@@ -2,4 +2,5 @@ export class User {
   constructor(public name: string, public bio: string, public age: number, public orientation: string, public imageUrl: string, public interests: string) {}
 
   public id: number
+  
 }
