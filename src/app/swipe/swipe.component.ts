f481f2178b3435056b96658ed182793e633c371b
Bobby Martin <robert.martin.619@gmail.com>
2018-06-21 10:26:35 -0700

    add hearted feature

--- src/app/swipe/swipe.component.ts
+++ src/app/swipe/swipe.component.ts
@@ -12,17 +12,24 @@ import { FilterPipe } from '../filter.pipe';
 })
 export class SwipeComponent implements OnInit {
   users;
+  user: User;
 
   constructor(private dataService: DataService) { }
 
   getUsers() {
     return this.dataService.getUsers().then(users => this.users = users);
   }
-  likeUser() {
-    const active = document.querySelector('.swiper');
-    // const pos = (num) => num += num;
-    const isActive = (num) => active.setAttribute('style', "transform: translateX(-" + (num += num) + '%)');
-    isActive(100);
+  likeUser(user) {
+    user.liked = true;
+    if(user.liked) {
+      const hearted = document.getElementById(user.id);
+      hearted.classList.toggle('been-hearted');
+    }
+
+    // const active = document.querySelector('.swiper');
+    // // const pos = (num) => num += num;
+    // const isActive = (num) => active.setAttribute('style', "transform: translateX(-" + (num += num) + '%)');
+    // isActive(100);
   }
 
   reject(user) {
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,6 +1,7 @@
 export class User {
   public id: number;
-  public rejected: boolean = true;
+  public rejected: boolean = false;
+  public liked: boolean = false;
   constructor(public name: string, public bio: string, public age: number, public orientation: string, public imageUrl: string, public interests: string) { }
 
 }
